6fa46f68da95b60dde61eafd14f2e0f41ae62699
Pranjalisingh8 <pranjalisin8@gmail.com>
2026-05-29 15:32:21 +0530

    Day1 uploaded

--- Day1/Question1.java
+++ Day1/Question1.java
@@ -3,13 +3,14 @@ package Day1;
 import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a number: ");
+            int n = sc.nextInt();
+            int sum = 0;
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+            System.out.println("The sum of first " + n + " natural numbers is: " + sum);
         }
-        System.out.println("The sum of first " + n + " natural numbers is: " + sum);
     }
 }
