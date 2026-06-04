cdc2c2ae00cb4d1dacf94ce440e4d26b898a4ab3
Pranjalisingh8 <pranjalisin8@gmail.com>
2026-06-04 15:54:52 +0530

    Day4 uploaded.

--- Day4/Question13.java
+++ Day4/Question13.java
@@ -1,5 +1,20 @@
+/*WAP TO GENERATE FIBONACCI SERIES */
 package Day4;
-
+import java.util.Scanner;
 public class Question13 {
-    
+    public static void main(String[] args) {
+        int n, a = 0, b = 1, c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of terms:");
+        n = sc.nextInt();
+        System.out.print("Fibonacci Series: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        sc.close();
+    }
+
 }
